dc6d13af49a22395d0a2c2f8c55c86db450fa657
Shane <shanemonck11@hotmail.com>
2024-08-17 16:28:57 +0800

    Got Web App to work with side car

--- ShanesStorage.Api/Program.cs
+++ ShanesStorage.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -26,9 +28,31 @@ app.MapGet("/weatherforecast", () =>
                                    return forecast;
                                });
 
+app.MapGet("", () => "Hello Shane, Ya Big boy");
+
+app.MapGet("/test/{id:int}", ([FromRoute] int id) => Results.Ok("You gave me this is: " + id));
+
+app.MapGet("/test", ([AsParameters] MyTestQuery query) => Results.Ok($"You gave me this ID: {query.Id} and Page: {query.Page}"));
+
+app.MapPost("/test", (MyTestPost myTestPost) => Results.Ok(myTestPost));
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public class MyTestQuery
+{
+    public string? Id { get; set; }
+
+    public int? Page { get; set; }
+}
+
+public class MyTestPost
+{
+    public string? Name { get; set; }
+
+    public int? Age { get; set; }
 }
